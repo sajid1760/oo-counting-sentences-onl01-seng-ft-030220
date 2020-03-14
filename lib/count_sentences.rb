5f38f1c11f7e69aaa64931b4fdbc716db5f8669b
Sajid Anjum <sajid1760@gmail.com>
2020-03-14 00:34:50 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,37 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    sentarray2 = []
+    sentarray3 = []
+    sentarray = self.split(".")
+    sentarray.each do |aa|
+      sentarray2 << aa.split("?")
+    end
+    puts sentarray2.inspect
+    sentarray2.each do |bb|
+      sentarray3 << bb.split("l")
+    end
+    a = 0 
+    sentarray3.each do |element|
+      if (element == "" || element.empty?) then sentarray.delete_at(a) end
+        a = a + 1
+    end
+    puts sentarray3.inspect
+    puts sentarray3.count
   end
+  
 end
+
+"Hello.. What's up? Well.".count_sentences
